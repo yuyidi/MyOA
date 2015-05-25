ff6ebbdd4d4904f0eedb00508706a5dc613fb78c
yuyidi <yuyidi0630@163.com>
2015-05-25 09:23:15 +0800

    测试

---
+++ src/test/java/com/yyd/myoa/tool/Testit.java
@@ -0,0 +1,51 @@
+package com.yyd.myoa.tool;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+
+public class Testit {
+	public static void main(String[] args) {
+		List<User> list = new ArrayList<User>();
+		User u1 = new User(1, 1, "f1");
+		User u2 = new User(2, 1, "f2");
+		User u3 = new User(3, 2, "f3");
+		User u7 = new User(7, 3, "f7");
+		User u4 = new User(4, 4, "f4");
+		User u5 = new User(5, 4, "f5");
+		User u6 = new User(6, 5, "f6");
+		list.add(u1);
+		list.add(u2);
+		list.add(u3);
+		list.add(u4);
+		list.add(u5);
+		list.add(u6);
+		list.add(u7);
+		Map<Integer, Map<Integer, List<User>>> map = new HashMap<Integer, Map<Integer, List<User>>>();
+		map = Testit.it(map, list);
+		System.out.println(JSON.toJSON(map));
+	}
+
+	public static Map<Integer, Map<Integer, List<User>>> it(Map<Integer, Map<Integer, List<User>>> map,
+			List <User> list) {
+		if (list != null) {
+			for (User user : list) {
+				if (user.getId() == user.getPid()) {
+					Map<Integer, List<User>> maplist = new HashMap<Integer, List<User>>();
+					map.put(user.getPid(), maplist);
+				} else {
+					if (map.get(user.getPid()) != null) {
+						List<User> listobj = new ArrayList<User>();
+						map.get(user.getPid()).put(user.getId(), listobj);
+					} else {
+						it(map, map.get(user.getPid()).get(user.getPid()));
+					}
+				}
+			}
+		}
+		return map;
+	}
+}
---
+++ src/test/java/com/yyd/myoa/tool/User.java
@@ -0,0 +1,44 @@
+package com.yyd.myoa.tool;
+
+public class User{
+	 private Integer id;
+	 private Integer pid;
+	 private String name;
+	 
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Integer getPid() {
+		return pid;
+	}
+
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public User(int id, int pid, String name) {
+		this.id = id;
+		this.pid = pid;
+		this.name = name;
+	}
+}
