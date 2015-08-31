e5479dc1a31ae8ac2dab2acf862b16d26fc88c5b
yuyidi <yuyidi0630@163.com>
2015-08-31 13:56:09 +0800

    可取客户端IP

--- src/main/java/com/yyd/myoa/utils/HttpUtils.java
+++ src/main/java/com/yyd/myoa/utils/HttpUtils.java
@@ -12,17 +12,20 @@ public class HttpUtils {
 	 * @return
 	 */
 	public static String getIp(HttpServletRequest request) {
-		String ip = request.getHeader("X-Real-IP");
-		if (StringUtils.isBlank(ip)) {
-			ip = request.getHeader("Host");
-		}
-		if (StringUtils.isBlank(ip)) {
-			ip = request.getHeader("X-Forwarded-For");
-		}
-		if (StringUtils.isBlank(ip)) {
-			ip = "0.0.0.0";
-		}
-		return ip;
+		 String ip = request.getHeader("x-forwarded-for");
+		    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		        ip = request.getHeader("Proxy-Client-IP");
+		    }
+		    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		        ip = request.getHeader("WL-Proxy-Client-IP");
+		    }
+		    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		        ip = request.getRemoteAddr();
+		    }
+		    if (ip.equals("0:0:0:0:0:0:0:1")) {
+		        ip = "0.0.0.0";
+		    }
+		    return ip;
 	}
 
 	/**
